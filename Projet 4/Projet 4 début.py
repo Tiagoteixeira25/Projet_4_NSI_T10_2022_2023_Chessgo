b9f3c403355cbb1d6c936a8945238de15cf35658
Tiago Teixeira <91455742+Tiagoteixeira25@users.noreply.github.com>
2023-01-31 04:59:40 -0500

    Add files via upload

---
+++ Projet 4/Projet 4 début.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 08:16:20 2023
+
+@author: teixeira
+"""
+
+# créer le mouvement des pièces
+
+def mouvement_pion():
+    pass
+def mouvement_reine():
+    pass
+def mouvement_roi():
+    pass
+def mouvement_cavalier():
+    pass
+def mouvement_fou():
+    pass
+def mouvement_tour():
+    pass
---
+++ Projet 4/untitled0.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 09:09:38 2023
+
+@author: teixeira
+"""
+
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+print(login())
